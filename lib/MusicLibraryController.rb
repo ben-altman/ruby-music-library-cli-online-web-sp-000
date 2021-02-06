0198d3199653181914f76f22c465eafaca845e89
Ben Altman <altmanguitar@gmail.com>
2021-02-06 04:26:53 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -83,7 +83,7 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     input = gets.strip.to_i
-    if song = list_songs[input- ]
+    if song = list_songs[input-1]
       puts "on it"
     end
   #  puts "playing #{list_songs[input-1]}"
