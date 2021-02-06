a4ec03ad7d77ddf38b9c75476e08a15058789398
Ben Altman <altmanguitar@gmail.com>
2021-02-06 03:10:50 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -25,8 +25,8 @@ class MusicLibraryController
     case input
       when 'list songs'
         Song.all.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
-        puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
-
+          puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+        end
     end
   end
 end
